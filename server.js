7a8f25ff3973d9cfcf95830b52f67ca353000da4
MarcusAGray <mrcsgry@gmail.com>
2022-06-27 22:32:40 +0100

    changed port alternative

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { Server } = require("socket.io");
 const httpServer = createServer(app);
 require('dotenv').config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 const io = new Server(httpServer, { });
 
